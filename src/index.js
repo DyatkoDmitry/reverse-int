32a51aab9f011f31899e8fdf615b88a5f1812b59
DyatkoDmitry <dyatkodmitry@gmail.com>
2022-01-13 23:13:43 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+
+// Get string integer without minus    
+let stringN = String(Math.abs(n)); 
+
+let reverseN = '';
+
+    for(let i = 0; i < stringN.length; i++){
+        reverseN = stringN[i] + reverseN;
+    }
+    return reverseN;
 }
+
